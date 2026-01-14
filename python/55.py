93a31c32af3d9608afde646eec66fa3907950999
prayagDev <88964135+prayagDev@users.noreply.github.com>
2026-01-14 22:32:05 +0530

    Create 55.py

---
+++ python/55.py
@@ -0,0 +1,7 @@
+# Merge two dictionaries
+
+dict1 = {'name': 'rahul', 'age': 20, 'id': 101}
+dict2 = {'surname': 'rawat', 'marital_status': 'Married', 'id': 101}
+merged = dict1 | dict2
+
+print(merged)
